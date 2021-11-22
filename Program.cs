60f37e9a63ba49b27ba180abd4c925061a63d690
EO <eo@MacBook-Air-EO.local>
2021-11-22 21:38:18 +0900

    start

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("2 3Hello, World!");
+Console.WriteLine("2 3Hello, World!");
+Console.WriteLine("2 3Hello, World!");
